88a64615822bfb397730cafc6b6236e62b24f2b4
PravinkumarZopsmart <pravin.kumar@zopsmart.com>
2022-04-30 22:14:41 +0530

    Update base url

--- src/components/Axios.js
+++ src/components/Axios.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const todoAPI = axios.create({
     withCredentials: true,
-    // baseURL: "http://localhost:4000"
+    baseURL: "https://pravintodoapp.herokuapp.com/"
 })
 
 export default todoAPI
