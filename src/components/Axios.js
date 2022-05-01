6ebc32b0430e1571a7392145c528b11fc67bb97b
PravinkumarZopsmart <pravin.kumar@zopsmart.com>
2022-05-01 18:46:04 +0530

    Change basurl in axios create

--- src/components/Axios.js
+++ src/components/Axios.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const todoAPI = axios.create({
     withCredentials: true,
-    baseURL: "https://pravintodoapp.herokuapp.com/"
+    baseURL: "https://pravintodoapp.herokuapp.com"
 })
 
 export default todoAPI
